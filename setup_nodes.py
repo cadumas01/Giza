420407e9cb73e60df17f60f510a8602dac17b8c9
cadumas01 <cadumas01@gmail.com>
2022-05-27 21:09:07 -0400

    update ssetupnodes

--- setup_nodes.py
+++ setup_nodes.py
@@ -39,6 +39,8 @@ server_names = config['server_names']
 # Going through each server and crunning setup_giza
 for server_name in server_names:
     server_url = get_machine_url(config, server_name)
-    run_remote_command_sync("./Giza/bash setup_giza.sh", server_url) # Not sure if works of if need to change directories or sudo su first
-    run_remote_command_sync("mkdir ./Giza/testfile2", server_url)
+
+    # When running remote commands, we are currently in root
+    run_remote_command_sync("bash ./Giza/setup_giza.sh", server_url) # Not sure if works of if need to change directories or sudo su first
+    # run_remote_command_sync("mkdir ./Giza/testfile2", server_url)
 
