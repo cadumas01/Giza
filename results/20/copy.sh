c6295e50e99f009dd65bc51702e298e84cfe3f54
cadumas01 <cadumas01@gmail.com>
2022-05-29 22:50:05 -0400

    done with cholula

---
+++ results/20/copy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+servers="../../servers.txt"
+i=1
+
+while IFS= read -r line
+do
+   echo "Here is $i"
+
+  port=${line% *}
+  server=${line##* }
+
+  rsync --rsh="$port" $server:~/Giza/latency.txt ./latency.$i.txt &
+  rsync --rsh="$port" $server:~/Giza/lattput.txt ./lattput.$i.txt &
+  i=$((i+1))
+ 
+
+done < "$servers"
+
+wait $(jobs -p)
